65968438b7a117dd84433c08cae41cc55e663491
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-08-28 21:24:03 +0530

    done

---
+++ ML  projects/happy.py
@@ -0,0 +1,9 @@
+import pyttsx3
+speech_function = pyttsx3.init()
+for i in range(): 
+    text =input()
+    if(text=='exit'):
+        exit()
+    else:
+        speech_function.say(text)
+        speech_function.runAndWait()
